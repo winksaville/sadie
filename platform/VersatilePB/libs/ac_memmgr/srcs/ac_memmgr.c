f8e245a296d9af00d89e631f6c6f288cc9ad9e33
Wink Saville <wink@saville.com>
2015-11-30 16:57:46 -0800

    Add test-ac_memmgr and fix bugs.

--- platform/VersatilePB/libs/ac_memmgr/srcs/ac_memmgr.c
+++ platform/VersatilePB/libs/ac_memmgr/srcs/ac_memmgr.c
@@ -16,9 +16,6 @@
 
 #include <ac_memmgr.h>
 
-/** Some memory */
-
-
 static ac_size_t idx = 0;
 
 #define MEM_ALIGN ((ac_size_t)64) // MUST BE POWER of 2
@@ -32,15 +29,20 @@ void* ac_malloc(ac_size_t size) {
   ac_size_t next_idx;
   ac_bool ok;
 
+  // We must return AC_NULL if size is 0
+  if (size == 0) {
+    return AC_NULL;
+  }
+
   // Roundup to next alignment factor
-  size = (size + MEM_ALIGN - 1) / MEM_ALIGN;
+  size = (size + MEM_ALIGN - 1) & ~(MEM_ALIGN - 1);
 
   // Loop until we can allocate or we have no more memory
   do {
     cur_idx = __atomic_load_n(&idx, __ATOMIC_ACQUIRE);
 
     next_idx = cur_idx + size;
-    if ((next_idx >= MAX_IDX) || (next_idx < cur_idx)) {
+    if ((next_idx >= MAX_IDX) || (next_idx <= cur_idx)) {
       return AC_NULL;
     }
     ok = __atomic_compare_exchange_n(&idx, &idx, next_idx,
---
+++ tests/test-ac_memmgr/srcs/test.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2015 Wink Saville
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ac_memmgr.h>
+
+#include <ac_test.h>
+
+ac_bool test_memmgr() {
+  ac_bool error = AC_FALSE;
+
+  // Must return AC_NULL although according to C99 standard a
+  // malloc(0) may return either but undefined behavior happens
+  // if the pointer is used. Therefore we'll defined it as always
+  // returning AC_NULL
+  error |= AC_TEST(ac_malloc(0) == AC_NULL,
+      "Expected ac_malloc(0) to return AC_NULL");
+
+  // Test conditions which attempt allocate too much memory
+  error |= AC_TEST(ac_malloc(((ac_size_t)0) - 1) == AC_NULL,
+      "Expected ac_malloc(-1) to always return AC_NULL");
+
+  error |= AC_TEST(ac_malloc(((ac_size_t)0) - 2) == AC_NULL,
+      "Expected ac_malloc(-2) to always return AC_NULL");
+
+  error |= AC_TEST(ac_malloc(((ac_size_t)0) - 63) == AC_NULL,
+      "Expected ac_malloc(-63) to always return AC_NULL");
+
+  error |= AC_TEST(ac_malloc(((ac_size_t)0) - 64) == AC_NULL,
+      "Expected ac_malloc(-64) to always return AC_NULL");
+
+  error |= AC_TEST(ac_malloc(((ac_size_t)0) - 65) == AC_NULL,
+      "Expected ac_malloc(-65) to always return AC_NULL");
+
+  error |= AC_TEST(ac_malloc(((ac_size_t)0) - 66) == AC_NULL,
+      "Expected ac_malloc(-66) to always return AC_NULL");
+
+  // Test conditions which must succeed as we expect at
+  // least being able to do a few small allocations
+  void* p1 = ac_malloc(1);
+  error |= AC_TEST(ac_malloc(1) != AC_NULL,
+      "Expected ac_malloc(1) to not be AC_NULL");
+  ac_free(p1);
+
+
+  void* p2 = ac_malloc(2);
+  error |= AC_TEST(p2 != AC_NULL,
+      "Expected ac_malloc(2) to not be AC_NULL");
+  ac_free(p2);
+
+
+  void* p63 = ac_malloc(63);
+  error |= AC_TEST(p63 != AC_NULL,
+      "Expected ac_malloc(63) to not be AC_NULL");
+  ac_free(p63);
+
+  void* p64 = ac_malloc(64);
+  error |= AC_TEST(p64 != AC_NULL,
+      "Expected ac_malloc(64) to not be AC_NULL");
+  ac_free(p64);
+
+  void* p65 = ac_malloc(1);
+  error |= AC_TEST(p65 != AC_NULL,
+      "Expected ac_malloc(65) to not be AC_NULL");
+  ac_free(p65);
+
+  return error;
+}
+
+int main(void) {
+    if (test_memmgr()) {
+        // Failed
+        ac_printf("ERR\n");
+        return 1;
+    } else {
+        // Succeeded
+        ac_printf("OK\n");
+        return 0;
+    }
+}
+
