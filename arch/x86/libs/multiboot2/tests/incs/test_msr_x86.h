9290719aa38e957812bf52f38161cdf2c355f17c
Wink Saville <wink@saville.com>
2016-02-12 09:23:48 -0800

    Add a arch/x86/libs subdirectory.

---
+++ arch/x86/libs/multiboot2/tests/incs/test_msr_x86.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2016 Wink Saville
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ARCH_X86_MSR_X86_TESTS_INCS_TEST_MSR_X86_H
+#define ARCH_X86_MSR_X86_TESTS_INCS_TEST_MSR_X86_H
+
+#include <ac_inttypes.h>
+
+ac_bool test_msr_apic_base_fields();
+
+ac_bool test_msr_mtrrcap_fields();
+
+#endif
