dad2b61172e7800cf61b754aae676e1b445abf78
Wink Saville <wink@saville.com>
2016-02-18 16:11:46 -0800

    Add test_pml4e_fields_array.

--- arch/x86/libs/page_table_x86/tests/srcs/test.c
+++ arch/x86/libs/page_table_x86/tests/srcs/test.c
@@ -171,6 +171,67 @@ static ac_bool test_cr3_pcide_paging_fields_array(void) {
   return error;
 }
 
+/**
+ * Test case array filled with the val followed
+ * by the expected field values. The array initialization
+ * fields that are NOT explicitly initialized are zeros.
+ *
+ * This is a shortened walking 1 bit test.
+ */
+static struct test_case_pml4e_fields {
+  union pml4e_fields_u val;
+  ac_u64 p;
+  ac_u64 rw;
+  ac_u64 us;
+  ac_u64 pwt;
+  ac_u64 pcd;
+  ac_u64 a;
+  ac_u64 reserved_0;
+  ac_u64 phy_addr;
+  ac_u64 xd;
+} test_case_pml4e_fields_array[] = {
+  { .val.raw=0x1, .p=0x1, },
+  { .val.raw=0x2, .rw=0x1, },
+  { .val.raw=0x4, .us=0x1, },
+  { .val.raw=0x8, .pwt=0x1, },
+  { .val.raw=0x10, .pcd=0x1, },
+  { .val.raw=0x20, .a=0x1, },
+  { .val.raw=0x40, .reserved_0=0x1, },
+  { .val.raw=0x800, .reserved_0=0x20, },
+  { .val.raw=0x1000, .phy_addr=0x1, },
+  { .val.raw=0x4000000000000000, .phy_addr=0x4000000000000, },
+  { .val.raw=0x8000000000000000, .xd=0x1, },
+};
+
+static ac_bool test_pml4e_fields(struct test_case_pml4e_fields* test) {
+  ac_bool error = AC_FALSE;
+
+  print_pml4e_fields("pml4e", test->val.raw);
+
+  error |= AC_TEST(test->val.fields.p == test->p);
+  error |= AC_TEST(test->val.fields.rw == test->rw);
+  error |= AC_TEST(test->val.fields.us == test->us);
+  error |= AC_TEST(test->val.fields.pwt == test->pwt);
+  error |= AC_TEST(test->val.fields.pcd == test->pcd);
+  error |= AC_TEST(test->val.fields.a == test->a);
+  error |= AC_TEST(test->val.fields.reserved_0 == test->reserved_0);
+  error |= AC_TEST(test->val.fields.phy_addr == test->phy_addr);
+  error |= AC_TEST(test->val.fields.xd == test->xd);
+
+  return error;
+}
+
+static ac_bool test_pml4e_fields_array(void) {
+  ac_bool error = AC_FALSE;
+
+  for (ac_uint i = 0; i < AC_ARRAY_COUNT(test_case_pml4e_fields_array); i++) {
+    error |= test_pml4e_fields(&test_case_pml4e_fields_array[i]);
+  }
+
+  return error;
+}
+
+
 int main(void) {
   ac_bool error = AC_FALSE;
 
@@ -181,6 +242,8 @@ int main(void) {
   error |= test_cr3_nrml_paging_fields_array();
   error |= test_cr3_pae_paging_fields_array();
   error |= test_cr3_pcide_paging_fields_array();
+
+  error |= test_pml4e_fields_array();
 
   if (!error) {
     ac_printf("OK\n");
