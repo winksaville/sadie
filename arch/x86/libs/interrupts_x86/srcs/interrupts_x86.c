aeaab343d251fce3af3f577ded22c4f1d881b1e0
Wink Saville <wink@saville.com>
2016-03-26 14:50:29 -0700

    Add missing interrupt handlers and have each output full saved regs.

--- arch/x86/libs/interrupts_x86/srcs/interrupts_x86.c
+++ arch/x86/libs/interrupts_x86/srcs/interrupts_x86.c
@@ -28,65 +28,29 @@
 /* Interrupt Descriptor Table */
 static intr_gate idt[256];
 
-static ac_u32 intr_undefined_counter;
-static ac_u32 expt_undefined_counter;
+static ac_u32 intr_divide_err_counter;
+static ac_u32 intr_debug_counter;
+static ac_u32 intr_nmi_counter;
+static ac_u32 intr_breakpoint_counter;
+static ac_u32 intr_overflow_counter;
+static ac_u32 intr_bound_range_counter;
+static ac_u32 intr_invalid_opcode_counter;
+static ac_u32 intr_device_not_available_counter;
 static ac_u32 expt_double_fault_counter;
+static ac_u32 intr_coprocessor_segment_overrun_counter;
 static ac_u32 expt_invalid_tss_counter;
 static ac_u32 expt_segment_not_present_counter;
 static ac_u32 expt_stack_fault_counter;
 static ac_u32 expt_general_protection_counter;
 static ac_u32 expt_page_fault_counter;
+static ac_u32 intr_x87_floating_point_counter;
 static ac_u32 expt_alignment_check_counter;
-static ac_u32 intr_invalid_opcode_counter;
+static ac_u32 intr_machine_check_counter;
+static ac_u32 intr_simd_floating_point_counter;
+static ac_u32 intr_virtualization_counter;
 
-INTERRUPT_HANDLER
-static void intr_undefined(struct intr_frame *frame) {
-  intr_undefined_counter += 1;
-  print_intr_frame("intr_undefined", frame);
-  ac_printf(" sp: %p\n", get_sp());
-  ac_printf(" intr_undefined_counter: %d\n", intr_undefined_counter);
-  reset_x86();
-}
-
-INTERRUPT_HANDLER
-static void expt_double_fault(struct intr_frame *frame, ac_uint error_code) {
-  expt_double_fault_counter += 1;
-  print_intr_frame("expt_double_fault - #DF 8", frame);
-  ac_printf(" rsp: %p\n", get_sp());
-  ac_printf(" error_code: 0x%x\n", error_code);
-  ac_printf(" expt_double_fault_counter: %d\n", expt_double_fault_counter);
-  reset_x86();
-}
-
-INTERRUPT_HANDLER
-static void expt_invalid_tss(struct intr_frame *frame, ac_uint error_code) {
-  expt_invalid_tss_counter += 1;
-  print_intr_frame("expt_invalid_tss - #TS 10", frame);
-  ac_printf(" rsp: %p\n", get_sp());
-  ac_printf(" error_code: 0x%x\n", error_code);
-  ac_printf(" expt_invalid_tss_counter: %d\n", expt_invalid_tss_counter);
-  reset_x86();
-}
-
-INTERRUPT_HANDLER
-static void expt_segment_not_present(struct intr_frame *frame, ac_uint error_code) {
-  expt_segment_not_present_counter += 1;
-  print_intr_frame("expt_segment_not_present - #NP 11", frame);
-  ac_printf(" rsp: %p\n", get_sp());
-  ac_printf(" error_code: 0x%x\n", error_code);
-  ac_printf(" expt_segment_not_present_counter: %d\n", expt_segment_not_present_counter);
-  reset_x86();
-}
-
-INTERRUPT_HANDLER
-static void expt_stack_fault(struct intr_frame *frame, ac_uint error_code) {
-  expt_stack_fault_counter += 1;
-  print_intr_frame("expt_stack_fault - #SS 12", frame);
-  ac_printf(" rsp: %p\n", get_sp());
-  ac_printf(" error_code: 0x%x\n", error_code);
-  ac_printf(" expt_stack_fault_counter: %d\n", expt_stack_fault_counter);
-  reset_x86();
-}
+static ac_u32 intr_undefined_counter;
+static ac_u32 expt_undefined_counter;
 
 #define NUM_SAVED_REGS 15
 struct saved_regs {
@@ -176,10 +140,184 @@ void print_full_intr_stack_frame(char* str, struct intr_frame* f) {
     ac_printf("%s:", str);
   }
   // Calculate the fsf subract saved_regs
-  struct full_expt_stack_frame* fsf = (void*)f - sizeof(struct saved_regs);
+  struct full_intr_stack_frame* fsf = (void*)f - sizeof(struct saved_regs);
   ac_printf("fsf=0x%p\n", fsf);
   print_saved_regs(&fsf->regs);
   print_intr_frame(AC_NULL, &fsf->iret_frame);
+}
+
+INTERRUPT_HANDLER
+static void intr_undefined(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#UD xx", frame);
+
+  intr_undefined_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_undefined_counter: %d\n", intr_undefined_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_divide_err(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#DE 0", frame);
+
+  intr_divide_err_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_divide_err_counter: %d\n", intr_divide_err_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_debug(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#DB 1", frame);
+
+  intr_debug_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_debug_counter: %d\n", intr_debug_counter);
+  reset_x86();
+}
+
+
+INTERRUPT_HANDLER
+static void intr_nmi(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#NMI 2", frame);
+
+  intr_nmi_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_nmi_counter: %d\n", intr_nmi_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_breakpoint(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#BP 3", frame);
+
+  intr_breakpoint_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_breakpoint_counter: %d\n", intr_breakpoint_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_overflow(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#OF 4", frame);
+
+  intr_overflow_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_overflow_counter: %d\n", intr_overflow_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_bound_range(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#BR 5", frame);
+
+  intr_bound_range_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_bound_range_counter: %d\n", intr_bound_range_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_invalid_opcode(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#UD 6", frame);
+
+  intr_invalid_opcode_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_invlid_opcode_counter: %d\n", intr_invalid_opcode_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_device_not_available(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#UD 7", frame);
+
+  intr_device_not_available_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_device_not_available_counter: %d\n", intr_device_not_available_counter);
+  reset_x86();
+}
+
+
+INTERRUPT_HANDLER
+static void expt_double_fault(struct intr_frame *frame, ac_uint error_code) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_expt_stack_frame("#DF 8", frame);
+
+  expt_double_fault_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" error_code: 0x%x\n", error_code);
+  ac_printf(" expt_double_fault_counter: %d\n", expt_double_fault_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_coprocessor_segment_overrun(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#CPSO 9", frame);
+
+  intr_coprocessor_segment_overrun_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" coprocessor_segment_overrun_counter: %d\n", intr_coprocessor_segment_overrun_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void expt_invalid_tss(struct intr_frame *frame, ac_uint error_code) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_expt_stack_frame("#TS 10", frame);
+
+  expt_invalid_tss_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" error_code: 0x%x\n", error_code);
+  ac_printf(" expt_invalid_tss_counter: %d\n", expt_invalid_tss_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void expt_segment_not_present(struct intr_frame *frame, ac_uint error_code) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_expt_stack_frame("#NP 11", frame);
+
+  expt_segment_not_present_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" error_code: 0x%x\n", error_code);
+  ac_printf(" expt_segment_not_present_counter: %d\n", expt_segment_not_present_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void expt_stack_fault(struct intr_frame *frame, ac_uint error_code) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#SS 12", frame);
+
+  expt_stack_fault_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" error_code: 0x%x\n", error_code);
+  ac_printf(" expt_stack_fault_counter: %d\n", expt_stack_fault_counter);
+  reset_x86();
 }
 
 INTERRUPT_HANDLER
@@ -200,8 +338,11 @@ static void expt_general_protection(struct intr_frame *frame, ac_uint error_code
 
 INTERRUPT_HANDLER
 static void expt_page_fault(struct intr_frame *frame, ac_uint error_code) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_expt_stack_frame("#PF 14", frame);
+
   expt_page_fault_counter += 1;
-  print_intr_frame("expt_page_fault - #PF 14", frame);
   ac_printf(" rsp: %p\n", get_sp());
   ac_printf(" error_code: 0x%x\n", error_code);
   ac_printf(" expt_page_fault_counter: %d\n", expt_page_fault_counter);
@@ -209,9 +350,24 @@ static void expt_page_fault(struct intr_frame *frame, ac_uint error_code) {
 }
 
 INTERRUPT_HANDLER
+static void intr_x87_floating_point(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#MF 16", frame);
+
+  intr_x87_floating_point_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_x87_floating_point_counter: %d\n", intr_x87_floating_point_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
 static void expt_alignment_check(struct intr_frame *frame, ac_uint error_code) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_expt_stack_frame("#AC 17", frame);
+
   expt_alignment_check_counter += 1;
-  print_intr_frame("expt_alignment_check - #AC 17", frame);
   ac_printf(" rsp: %p\n", get_sp());
   ac_printf(" error_code: 0x%x\n", error_code);
   ac_printf(" expt_alignment_check_counter: %d\n", expt_alignment_check_counter);
@@ -219,11 +375,38 @@ static void expt_alignment_check(struct intr_frame *frame, ac_uint error_code) {
 }
 
 INTERRUPT_HANDLER
-static void intr_invalid_opcode(struct intr_frame *frame) {
-  intr_invalid_opcode_counter += 1;
-  print_intr_frame("intr_invalid_opcode - #UD 6", frame);
+static void intr_machine_check(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#MC 18", frame);
+
+  intr_machine_check_counter += 1;
   ac_printf(" rsp: %p\n", get_sp());
-  ac_printf(" intr_invalid_opcode_counter: %d\n", intr_invalid_opcode_counter);
+  ac_printf(" intr_machine_check_counter: %d\n", intr_machine_check_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_simd_floating_point(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_intr_stack_frame("#XM 19", frame);
+
+  intr_simd_floating_point_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_simd_floating_point_counter: %d\n", intr_simd_floating_point_counter);
+  reset_x86();
+}
+
+INTERRUPT_HANDLER
+static void intr_virtualization(struct intr_frame *frame) {
+  __asm__ volatile(""::: "rax", "rbx", "rcx", "rdx", "rsi", "rdi", // "rbp", rbp is already saved
+                         "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15");
+  print_full_expt_stack_frame("#VE 20", frame);
+
+  intr_virtualization_counter += 1;
+  ac_printf(" rsp: %p\n", get_sp());
+  ac_printf(" intr_virtualization_counter: %d\n", intr_virtualization_counter);
   reset_x86();
 }
 
@@ -282,29 +465,52 @@ intr_gate* get_intr_gate(ac_u32 intr_num) {
 void initialize_intr_descriptor_table(void) {
   intr_undefined_counter = 0;
   expt_undefined_counter = 0;
+
+  intr_divide_err_counter = 0;
+  intr_debug_counter = 0;
+  intr_nmi_counter = 0;
+  intr_breakpoint_counter = 0;
+  intr_overflow_counter = 0;
+  intr_bound_range_counter = 0;
+  intr_invalid_opcode_counter = 0;
   expt_double_fault_counter = 0;
   expt_invalid_tss_counter = 0;
   expt_segment_not_present_counter = 0;
   expt_stack_fault_counter = 0;
   expt_general_protection_counter = 0;
   expt_page_fault_counter = 0;
+  intr_x87_floating_point_counter = 0;
   expt_alignment_check_counter = 0;
-  intr_invalid_opcode_counter = 0;
+  intr_machine_check_counter = 0;
+  intr_simd_floating_point_counter = 0;
+  intr_virtualization_counter = 0;
 
   // Initialize all of the entires to intr_undefined
   for (ac_u64 intr_num = 0; intr_num < AC_ARRAY_COUNT(idt); intr_num++) {
     set_intr_gate(&idt[intr_num], intr_undefined);
   }
+  // Initialize interrupts and exceptions
+  set_intr_gate(&idt[0], intr_divide_err);
+  set_intr_gate(&idt[1], intr_debug);
+  set_intr_gate(&idt[2], intr_nmi);
+  set_intr_gate(&idt[3], intr_breakpoint);
+  set_intr_gate(&idt[4], intr_overflow);
+  set_intr_gate(&idt[5], intr_bound_range);
   set_intr_gate(&idt[6], intr_invalid_opcode);
-
-  // Initialize some exceptions
-  set_expt_gate(&idt[8],  expt_double_fault);
+  set_intr_gate(&idt[7], intr_device_not_available);
+  set_expt_gate(&idt[8], expt_double_fault);
+  set_intr_gate(&idt[9], intr_coprocessor_segment_overrun);
   set_expt_gate(&idt[10], expt_invalid_tss);
   set_expt_gate(&idt[11], expt_segment_not_present);
   set_expt_gate(&idt[12], expt_stack_fault);
   set_expt_gate(&idt[13], expt_general_protection);
   set_expt_gate(&idt[14], expt_page_fault);
+
+  set_intr_gate(&idt[16], intr_x87_floating_point);
   set_expt_gate(&idt[17], expt_alignment_check);
+  set_intr_gate(&idt[18], intr_machine_check);
+  set_intr_gate(&idt[19], intr_simd_floating_point);
+  set_intr_gate(&idt[20], intr_virtualization);
 
   // set the idt register to point to idt array
   set_idtr(idt, AC_ARRAY_COUNT(idt));
