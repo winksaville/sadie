eaff3df82f00a1c77ff3ed732d5db87111607baa
Wink Saville <wink@saville.com>
2016-04-08 10:19:19 -0700

    Add ac_memcpy.
    A simple unoptimized memory copy.

---
+++ libs/ac_memcpy/srcs/ac_memcpy.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Wink Saville
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ac_memcpy.h>
+
+#include <ac_inttypes.h>
+
+/**
+ * copy unsigned 8 bit bytes from src to dest
+ *
+ * @param dest points to the destination array
+ * @param src points to the source array
+ * @param count number of bytes to copy
+ */
+void ac_memcpy(void *dest, const void* src, ac_size_t count) {
+  ac_u8* pd = dest;
+  const ac_u8* ps = src;
+  while (count-- != 0) {
+    *pd++ = *ps++;
+  }
+}
+
