b751e2534b0bac889886ccb330edbe564d3dc541
Wink Saville <wink@saville.com>
2016-05-09 20:09:39 -0700

    WIP: Update test_thread_dispatching.
    This is running, but the message isn't being processed.

--- tests/test-ac_dispatcher/srcs/test_threaded_dispatching.c
+++ tests/test-ac_dispatcher/srcs/test_threaded_dispatching.c
@@ -23,12 +23,13 @@
 #include <ac_inttypes.h>
 #include <ac_mpscfifo.h>
 #include <ac_debug_printf.h>
+#include <ac_receptor.h>
 #include <ac_test.h>
 
-static ac_bool ac1_process_msg(ac* this, ac_msg* pmsg) {
+static ac_bool t1_process_msg(ac* this, ac_msg* pmsg) {
   ac_bool error = AC_FALSE;
 
-  ac_debug_printf("ac1_process_msg:+ pmsg->cmd=%d, pmsg->arg=%d\n",
+  ac_debug_printf("t1_process_msg:+ pmsg->cmd=%d, pmsg->arg=%d\n",
       pmsg->cmd, pmsg->arg);
 
   error |= AC_TEST(pmsg->cmd == 1);
@@ -36,22 +37,81 @@ static ac_bool ac1_process_msg(ac* this, ac_msg* pmsg) {
 
   pmsg->arg = (ac_u32)error;
 
-  ac_debug_printf("ac1_process_msg:- pmsg->cmd=%d, pmsg->arg=%d\n",
+  ac_debug_printf("t1_process_msg:- pmsg->cmd=%d, pmsg->arg=%d\n",
       pmsg->cmd, pmsg->arg);
 
   return AC_TRUE;
 }
 
-static void * ac1_get_data(ac* this) {
+static void * t1_get_data(ac* this) {
   return AC_NULL;
 }
 
-static ac ac1 = {
-  .process_msg = &ac1_process_msg,
-  .get_data = &ac1_get_data,
+static ac t1_ac = {
+  .process_msg = &t1_process_msg,
+  .get_data = &t1_get_data,
 };
 
+static ac_mpscfifo t1_acq;
+static ac_bool t1_done;
+static ac_receptor_t t1_receptor_ready;
+static ac_receptor_t t1_receptor_done;
+static ac_receptor_t t1_receptor_waiting;
 
+void* t1(void *param) {
+  ac_bool error = AC_FALSE;
+
+  // Add an acq
+  ac_msg msg_stub;
+  ac_dispatcher* d;
+
+  t1_receptor_waiting = ac_receptor_create(AC_FALSE);
+
+  // Get a dispatcher
+  d = ac_dispatcher_get(1);
+  error |= AC_TEST(d != AC_NULL);
+
+  // Init the queue
+  ac_init_mpscfifo(&t1_acq, &msg_stub);
+
+  // Add ac1 and its Q dispatcher
+  ac_dispatcher_add_acq(d, &t1_ac, &t1_acq);
+
+  // Not done
+  __atomic_store_n(&t1_done, AC_FALSE, __ATOMIC_RELEASE);
+
+  // Signal t1 is ready
+  ac_debug_printf("t1: ready\n");
+  ac_receptor_signal(t1_receptor_ready, AC_TRUE);
+
+  // Continuously dispatch messages until done
+  ac_debug_printf("t1: looping\n");
+  while (__atomic_load_n(&t1_done, __ATOMIC_ACQUIRE) == AC_FALSE) {
+    if (!ac_dispatch(d)) {
+      ac_debug_printf("t1: waiting\n");
+      ac_receptor_wait(t1_receptor_waiting);
+    }
+  }
+
+  if (error) {
+    ac_debug_printf("t1: error\n");
+  }
+
+  ac_debug_printf("t1: done\n");
+
+  ac_receptor_signal(t1_receptor_done, AC_TRUE);
+  return AC_NULL;
+}
+
+void t1_add_msg(ac_msg* msg) {
+  ac_add_msg(&t1_acq, msg);
+  ac_receptor_signal(t1_receptor_waiting, AC_FALSE);
+}
+
+void t1_mark_done(void) {
+  __atomic_store_n(&t1_done, AC_TRUE, __ATOMIC_RELEASE);
+  ac_receptor_signal(t1_receptor_waiting, AC_TRUE);
+}
 
 /**
  * Test threaded dispatching a message
@@ -62,43 +122,31 @@ ac_bool test_threaded_dispatching() {
   ac_bool error = AC_FALSE;
   ac_debug_printf("test_threaded_dispatching:+\n");
 
-  // Get a dispatcher
-  ac_dispatcher* pd = ac_dispatcher_get(1);
-  error |= AC_TEST(pd != AC_NULL);
+  // Change ac_thread_init so we don't have to account for "system" threads
+  ac_thread_init(3);
 
-  // Add an acq
-  ac* pac1;
-  ac_mpscfifo ac1q;
-  ac_msg stub1;
+  t1_receptor_ready = ac_receptor_create(AC_FALSE);
+  t1_receptor_done = ac_receptor_create(AC_FALSE);
 
-  ac_mpscfifo* pac1q = ac_init_mpscfifo(&ac1q, &stub1);
-  error |= AC_TEST(pac1q == &ac1q);
+  ac_thread_rslt_t result = ac_thread_create(AC_THREAD_STACK_MIN, t1, AC_NULL);
+  error |= AC_TEST(result.status == 0);
 
-  pac1 = ac_dispatcher_add_acq(pd, &ac1, &ac1q);
-  error |= AC_TEST(pac1 != AC_NULL);
+  // Wait for t1 to be ready
+  ac_receptor_wait(t1_receptor_ready);
 
-  // Initialize message and add it to queue
+  // Send a message
+#if AC_TRUE
   ac_msg msg1 = {
     .cmd = 1,
     .arg = 2
   };
-  ac_add_msg(pac1q, &msg1);
+  t1_add_msg(&msg1);
+#endif
 
+  t1_mark_done();
 
-  ac_debug_printf("test_threaded_dispatching: dispatch now\n");
-  ac_bool processed_msgs = ac_dispatch(pd);
-  ac_debug_printf("test_threaded_dispatching: dispatch complete\n");
-  error |= AC_TEST(processed_msgs == AC_TRUE);
-
-  ac_msg msg2 = {
-    .cmd = 1,
-    .arg = 3
-  };
-  ac_add_msg(pac1q, &msg2);
-
-  ac_debug_printf("test_threaded_dispatching: rmv_ac\n");
-  ac* pac2 = ac_dispatcher_rmv_ac(pd, pac1);
-  error |= AC_TEST(pac2 == pac1);
+  // Wait for t1 to be done
+  ac_receptor_wait(t1_receptor_done);
 
   ac_debug_printf("test_threaded_dispatching:- error=%d\n", error);
   return error;
