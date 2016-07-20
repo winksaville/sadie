07066935f89d447294b0dfcb9db58180989b7fdd
Wink Saville <wink@saville.com>
2016-07-20 10:47:41 -0700

    Initial network link layer support, AcNetLl.
    Currently does nothing and only implemented in platform/Posix, I expect
    it to evolve there are not messages or API defined yet.

---
+++ platform/Posix/libs/ac_net_ll_impl/srcs/ac_net_ll_impl.c
@@ -0,0 +1,30 @@
+/*
+ * copyright 2016 wink saville
+ *
+ * licensed under the apache license, version 2.0 (the "license");
+ * you may not use this file except in compliance with the license.
+ * you may obtain a copy of the license at
+ *
+ *     http://www.apache.org/licenses/license-2.0
+ *
+ * unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "as is" basis,
+ * without warranties or conditions of any kind, either express or implied.
+ * see the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+#define NDEBUG
+
+#include <ac_net_ll_impl.h>
+
+
+#include <ac_debug_printf.h>
+
+
+/**
+ * Initialize this module
+ */
+void AcNetLl_init(void) {
+  ac_debug_printf("AcNetLl_init:#\n");
+}
---
+++ platform/Posix/libs/ac_net_ll_impl/tests/srcs/test.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Wink Saville
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ac_net_ll_impl.h>
+
+#include <ac_inttypes.h>
+#include <ac_printf.h>
+#include <ac_test.h>
+
+ac_uint test_net_ll(void) {
+  ac_uint error = AC_FALSE;
+
+  return error;
+}
+
+int main(void) {
+  ac_uint error = AC_FALSE;
+
+  AcNetLl_init();
+
+  if (!error) {
+    error |= test_net_ll();
+  }
+
+  if (!error) {
+    ac_printf("OK\n");
+  }
+
+  return error;
+}
