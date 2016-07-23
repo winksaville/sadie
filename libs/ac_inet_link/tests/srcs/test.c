4ca6adf4cf66c4644c317295e1e14cce336055da
Wink Saville <wink@saville.com>
2016-07-22 17:09:45 -0700

    Add tests
    Added test_hton_be and test_hton_le to test AC_HTON_XXX with big and little
    endian.
    
    Added trivial zero test in test_AcNetIpv4FragmentOffset.

--- libs/ac_inet_link/tests/srcs/test.c
+++ libs/ac_inet_link/tests/srcs/test.c
@@ -16,10 +16,73 @@
 
 #include <ac_inet_link.h>
 
+#include <ac_memset.h>
 #include <ac_printf.h>
+#include <ac_test.h>
 
-ac_bool test_AcInetFrament_ipv6_hdr(void) {
+#pragma push_macro("AC_ARCH_BYTE_ORDER")
+#undef AC_ARCH_BYTE_ORDER
+#undef AC_HTON_U16
+#undef AC_HTON_U32
+#undef AC_HTON_U64
+#define AC_ARCH_BYTE_ORDER AC_ARCH_BYTE_ORDER_BIG_ENDIAN
+#undef SADIE_LIBS_AC_INET_LINK_INCS_AC_INET_HTON_H
+#include <ac_inet_hton.h>
+
+ac_bool test_hton_be(void) {
   ac_bool error = AC_FALSE;
+
+  ac_u16 a_u16 = 0x0123;
+  ac_u32 a_u32 = 0x01234567;
+  ac_u64 a_u64 = 0x0123456789ABCDEF;
+
+  // Big endian should be unmodified
+  error |= AC_TEST(AC_HTON_U16(a_u16) == a_u16);
+  error |= AC_TEST(AC_HTON_U32(a_u32) == a_u32);
+  error |= AC_TEST(AC_HTON_U64(a_u64) == a_u64);
+
+  return error;
+}
+#pragma pop_macro("AC_ARCH_BYTE_ORDER")
+
+#pragma push_macro("AC_ARCH_BYTE_ORDER")
+#undef AC_ARCH_BYTE_ORDER
+#undef AC_HTON_U16
+#undef AC_HTON_U32
+#undef AC_HTON_U64
+#define AC_ARCH_BYTE_ORDER AC_ARCH_BYTE_ORDER_LITTLE_ENDIAN
+#undef SADIE_LIBS_AC_INET_LINK_INCS_AC_INET_HTON_H
+#include <ac_inet_hton.h>
+
+ac_bool test_hton_le(void) {
+  ac_bool error = AC_FALSE;
+
+  ac_u16 a_u16 = 0x0123;
+  ac_u32 a_u32 = 0x01234567;
+  ac_u64 a_u64 = 0x0123456789ABCDEF;
+
+  // Little endian should be swapped
+  error |= AC_TEST(AC_HTON_U16(a_u16) == AC_SWAP_BYTES_U16(a_u16));
+  error |= AC_TEST(AC_HTON_U32(a_u32) == AC_SWAP_BYTES_U32(a_u32));
+  error |= AC_TEST(AC_HTON_U64(a_u64) == AC_SWAP_BYTES_U64(a_u64));
+
+  return error;
+}
+#pragma pop_macro("AC_ARCH_BYTE_ORDER")
+
+ac_bool test_AcInetIpv4FragmentOffset(void) {
+  ac_bool error = AC_FALSE;
+
+  AcInetIpv4FragmentOffset frag;
+
+  ac_memset(&frag, 0, sizeof(frag));
+  error |= AC_TEST(frag.raw_u8[0] == 0);
+  error |= AC_TEST(frag.raw_u8[1] == 0);
+  error |= AC_TEST(frag.raw_u16 == 0);
+  error |= AC_TEST(frag.offset_in_u64s == 0);
+  error |= AC_TEST(frag.more_fragments == 0);
+  error |= AC_TEST(frag.dont_fragment == 0);
+  error |= AC_TEST(frag.zero == 0);
 
   return error;
 }
@@ -33,6 +96,9 @@ ac_bool test_inet_link(void) {
 int main(void) {
   ac_bool error = AC_FALSE;
 
+  error |= test_hton_be();
+  error |= test_hton_le();
+  error |= test_AcInetIpv4FragmentOffset();
   error |= test_inet_link();
 
   if (!error) {
